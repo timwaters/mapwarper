a9cd3edfcda046e22d5fae4d2d1336f302557d3c
Tim Waters <chippy2005@gmail.com>
2010-07-30 16:02:16 +0100

    use nokogiri instead of rexml for best guess places

--- app/models/map.rb
+++ app/models/map.rb
@@ -3,6 +3,7 @@ require "ftools"
 require "matrix"
 require 'erb'
 require 'rexml/document'
+require 'nokogiri'
 include ErrorCalculator
 
 
@@ -737,7 +738,17 @@ class Map < ActiveRecord::Base
    def find_bestguess_places
      url = URI.parse('http://wherein.yahooapis.com/v1/document')
      appid = Yahoo_app_id
-     documentContent = "<xml><title>#{REXML::Text.new(self.title).to_s}</title><description>#{REXML::Text.new( self.description ).to_s}</description></xml>"
+     builder = Nokogiri::XML::Builder.new do |xml|
+       xml.root {
+         xml.title {
+           xml.text self.title.to_s
+         }
+         xml.description {
+           xml.text self.description.to_s
+         }
+       }
+     end
+     documentContent = builder.to_xml
      documentType = "text/xml"
      focusWoeid = 2459115  #focus to new york
      post_args = {
@@ -748,40 +759,45 @@ class Map < ActiveRecord::Base
      }
      begin
        resp, data = Net::HTTP.post_form(url, post_args)
-       logger.debug data.inspect
-       @results = REXML::Document.new(data)
-       xmldoc = @results.root
-       if xmldoc.elements["document"] && xmldoc.elements["document"].size > 1
-         south_west = [xmldoc.elements["document/extents/southWest/longitude"].text, xmldoc.elements["document/extents/southWest/latitude"].text]
-         north_east = [xmldoc.elements["document/extents/northEast/longitude"].text, xmldoc.elements["document/extents/northEast/latitude"].text]
+       
+       @newresults = Nokogiri::XML.parse(data)
+       xmlroot = @newresults.root
+       if xmlroot.at('document') && xmlroot.at('document').children.size > 1
+         south_west = [xmlroot.at('document > extents > southWest > longitude').children[0].text,
+           xmlroot.at('document > extents > southWest > latitude').children[0].text]
+         north_east = [xmlroot.at('document > extents > northEast > longitude').children[0].text,
+           xmlroot.at('document > extents > northEast > latitude').children[0].text]
          extents = [south_west + north_east].join ','
 
          places = Array.new
          place_woeids = Array.new
          place_count = 0
-         xmldoc.elements.each('document/placeDetails/place') do | place |
+         xmlroot.xpath('/xmlns:contentlocation/xmlns:document/xmlns:placeDetails/xmlns:place').each do | place |
            place_hash = Hash.new
-         place_hash[:name] = place.elements["name"].text
-         place_hash[:lon] = place.elements["centroid/longitude"].text
-         place_hash[:lat] = place.elements["centroid/latitude"].text
-         places << place_hash
-         place_woeids << place.elements["woeId"].text
-         place_count += 1
+           place_hash[:name] = place.at('name').children[0].text
+           place_hash[:lon] = place.at('centroid > longitude').children[0].text
+           place_hash[:lat] = place.at('centroid > latitude').children[0].text
+           places << place_hash
+           place_woeids << place.at('woeId').children[0].text
+           place_count += 1
          end
+
          if !self.layers.visible.empty? && !self.layers.visible.first.maps.warped.empty?
            sibling_extent = self.layers.visible.first.maps.warped.last.bbox
          else
            sibling_extent = nil
          end
          top_places = Array.new
-         xmldoc.elements.each('document/referenceList/reference') do | reference |
-           if "/xml[1]/title[1]" ==  reference.elements["xpath"].text
-             ref_woeid =  reference.elements["woeIds"].text.split.first
+
+         xmlroot.xpath('/xmlns:contentlocation/xmlns:document/xmlns:referenceList/xmlns:reference').each do | reference |
+           if "/root[1]/title[1]" ==  reference.at('xpath').children[0].text
+             ref_woeid =  reference.at('woeIds').children[0].text.split.first
              if place_woeids.include? ref_woeid
                top_places <<  places[place_woeids.index(ref_woeid)]
              end
            end
          end
+
          temp_a = places - top_places
          places = top_places + temp_a
          placemaker_result = {:status => "ok", :map_id => self.id, :extents => extents, :count => place_count, :places => places, :sibling_extent=> sibling_extent}
@@ -789,12 +805,11 @@ class Map < ActiveRecord::Base
        else
          placemaker_result = {:status => "fail", :code => "no results"}
        end
+             
+      
      rescue SocketError => e
        logger.error "Socket error in find bestguess places" + e.to_s
        placemaker_result = {:status => "fail", :code => "socketError"}
-     rescue REXML::ParseException => e
-       logger.error "Error parsing XML " + e.to_s
-       placemaker_result = {:status => "fail", :code => "Rexml Error"}
      end
      placemaker_result
    end
