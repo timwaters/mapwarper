6ee2244ff0615287c95e20a9d5ae1297246d11f8
Tim Waters <chippy2005@gmail.com>
2013-07-31 13:02:38 +0100

    reverting last commit - undo adding depicts_year to maps

--- db/migrate/20130731112504_add_depicts_year_to_maps.rb
+++
@@ -1,9 +0,0 @@
-class AddDepictsYearToMaps < ActiveRecord::Migration
-  def self.up
-    add_column :maps, :depicts_year, :string
-  end
-
-  def self.down
-    remove_column :maps, :depicts_year
-  end
-end
