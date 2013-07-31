82ce723f91be647afb3557629d5f50b754bc03ee
Tim Waters <chippy2005@gmail.com>
2013-07-31 13:15:43 +0100

    add depicts year to map json geosearch and search/index responses

--- app/controllers/maps_controller.rb
+++ app/controllers/maps_controller.rb
@@ -164,7 +164,7 @@ class MapsController < ApplicationController
     end
 
     paginate_params = {
-      :select => "bbox, title, description, updated_at, id",
+      :select => "bbox, title, description, updated_at, id, date_depicted",
       :page => params[:page],
       :per_page => 20,
       :order => sort_geo + sort_clause + sort_nulls,
@@ -180,7 +180,7 @@ class MapsController < ApplicationController
         :per_page => @maps.per_page,
         :total_entries => @maps.total_entries,
         :total_pages => @maps.total_pages,
-        :items => @maps.to_a}.to_json , :callback => params[:callback]}
+        :items => @maps.to_a}.to_json(:methods => :depicts_year) , :callback => params[:callback]}
     end
   end
 
@@ -281,7 +281,7 @@ class MapsController < ApplicationController
         render :action => 'index.rjs'
       else
         respond_to do |format|
-          format.html{ render :layout =>'application' }  # index.html.erb
+        format.html{ render :layout =>'application' }  # index.html.erb
         format.xml  { render :xml => @maps.to_xml(:root => "maps", :except => [:content_type, :size, :bbox_geom, :uuid, :parent_uuid, :filename, :parent_id,  :map, :thumbnail, :rough_centroid]) {|xml|
         xml.tag!'stat', "ok"
         xml.tag!'total-entries', @maps.total_entries
@@ -293,7 +293,7 @@ class MapsController < ApplicationController
           :per_page => @maps.per_page,
           :total_entries => @maps.total_entries,
           :total_pages => @maps.total_pages,
-          :items => @maps.to_a}.to_json(:except => [:content_type, :size, :bbox_geom, :uuid, :parent_uuid, :filename, :parent_id,  :map, :thumbnail, :rough_centroid]) , :callback => params[:callback]
+          :items => @maps.to_a}.to_json(:except => [:content_type, :size, :bbox_geom, :uuid, :parent_uuid, :filename, :parent_id,  :map, :thumbnail, :rough_centroid], :methods => :depicts_year) , :callback => params[:callback]
         }
         end
       end
