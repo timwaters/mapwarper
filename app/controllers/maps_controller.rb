569f52bd06e702cf5464e48ec9535ef8d8522b57
Tim Waters <chippy2005@gmail.com>
2014-07-16 18:08:13 +0100

    Get basic paperclip upload functionality working

--- app/controllers/maps_controller.rb
+++ app/controllers/maps_controller.rb
@@ -7,4 +7,47 @@ class MapsController < ApplicationController
     @maps = Map.all
   end
 
+  def new
+    @map = Map.new
+    @html_title = "Upload a new map to "
+    @max_size = Map.max_attachment_size
+    if Map.max_dimension
+      @upload_file_message  = " It may resize the image if it's too large (#{Map.max_dimension}x#{Map.max_dimension}) "
+    else
+      @upload_file_message = ""
+    end
+
+    respond_to do |format|
+      format.html{ render :layout =>'application' }  # new.html.erb
+      format.xml  { render :xml => @map }
+    end
+  end
+  
+  def create
+    logger.debug params.inspect
+    logger.debug params[:map].inspect
+    @map = Map.new(map_params)
+
+    if user_signed_in?
+      @map.owner = current_user
+      @map.users << current_user
+    end
+
+    respond_to do |format|
+      if @map.save
+        flash[:notice] = 'Map was successfully created.'
+        format.html { redirect_to(@map) }
+        format.xml  { render :xml => @map, :status => :created, :location => @map }
+      else
+        format.html { render :action => "new", :layout =>'application' }
+        format.xml  { render :xml => @map.errors, :status => :unprocessable_entity }
+      end
+    end
+  end
+  
+   private
+    def map_params
+      params.require(:map).permit!
+    end
+  
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -41,4 +41,17 @@ module ApplicationHelper
     str.gsub(/[\]\[()]/,"")
   end
   
+  def error_messages_for(*objects)
+    options = objects.extract_options!
+    options[:header_message] ||= I18n.t(:"activerecord.errors.header", :default => "Invalid Fields")
+    options[:message] ||= I18n.t(:"activerecord.errors.message", :default => "Correct the following errors and try again.")
+    messages = objects.compact.map { |o| o.errors.full_messages }.flatten
+    unless messages.empty?
+      content_tag(:div, :class => "error_messages") do
+        list_items = messages.map { |msg| content_tag(:li, msg) }
+        content_tag(:h2, options[:header_message]) + content_tag(:p, options[:message]) + content_tag(:ul, list_items.join.html_safe)
+      end
+    end
+end
+  
 end
