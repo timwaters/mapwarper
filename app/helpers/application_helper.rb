7f826276faed969819d77bc1f79ee8081efe6170
Tim Waters <chippy2005@gmail.com>
2014-07-22 15:13:13 +0100

    fix rails 4 flash messages helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -6,13 +6,21 @@ module ApplicationHelper
   
   FLASH_NOTICE_KEYS = [:error, :notice, :warning]
   def flash_messages
-    return unless messages = flash.keys.select{|k| FLASH_NOTICE_KEYS.include?(k)}
+    return unless messages = flash.keys.select{|k| FLASH_NOTICE_KEYS.include?(k.to_sym)}
     formatted_messages = messages.map do |type|      
       content_tag :div, :id => type.to_s do
         message_for_item(flash[type], flash["#{type}_item".to_sym])
       end
     end
-    formatted_messages.join
+    formatted_messages.join.html_safe
+  end
+  
+  def message_for_item(message, item = nil)
+    if item.is_a?(Array)
+      message % link_to(*item)
+    else
+      message % item
+    end
   end
   
   #from rails way
