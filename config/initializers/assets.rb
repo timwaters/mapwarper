b5eddf0b9715220f0df58a4e7eda8942c6a9789e
Tim Waters <chippy2005@gmail.com>
2014-07-27 22:43:26 +0100

    Port over warped / preview to Rails 4

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -5,4 +5,4 @@ Rails.application.config.assets.version = '1.0'
 
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
- Rails.application.config.assets.precompile += %w(align.js clip.js warp.js openlayers/2.8/OpenLayers-2.8/OpenLayers.js )
+ Rails.application.config.assets.precompile += %w(warped.js align.js clip.js warp.js openlayers/2.8/OpenLayers-2.8/OpenLayers.js )
