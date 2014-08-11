d29231fc222a33e8256e8efc9b375c8bfe95c0a0
Tim Waters <chippy2005@gmail.com>
2014-08-11 18:04:10 +0200

    change mail initialiser to use sendmail

--- config/initializers/mail.rb
+++ config/initializers/mail.rb
@@ -1,9 +1,2 @@
 # Email settings
-ActionMailer::Base.delivery_method = :smtp
-ActionMailer::Base.smtp_settings = {
-  :address => "mail.geothings.net",
-  :port => 25,
-  :domain => "warper.geothings.net",
-  :username => ""
-
-}
+ActionMailer::Base.delivery_method = :sendmail
