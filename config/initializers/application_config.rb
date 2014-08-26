5cc4db7879f0c075ac830957319bafefcbd730b2
Tim Waters <chippy2005@gmail.com>
2014-08-26 14:42:15 +0100

    Tidy up and remove some constants in favour of app_config, and set up defaults properly for app_config paths

--- config/initializers/application_config.rb
+++ config/initializers/application_config.rb
@@ -2,18 +2,24 @@ CONFIG_PATH="#{Rails.root}/config/application.yml"
 
 APP_CONFIG = YAML.load_file(CONFIG_PATH)[Rails.env]
 
-ADDTHIS_TWITTER_USER = APP_CONFIG['addthis_twitter_user']
-ADDTHIS_USER = APP_CONFIG['addthis_user']
-GOOGLE_ANALYTICS_CODE  = APP_CONFIG['google_analytics_code']
-GOOGLE_ANALYTICS_COOKIE_PATH = APP_CONFIG['google_analytics_cookie_path']
-DST_MAPS_DIR = File.join(Rails.root, APP_CONFIG['dst_maps_dir'])
-SRC_MAPS_DIR =  File.join(Rails.root, APP_CONFIG['src_maps_dir'])
+#directories for maps and layer/mosaic tileindex shapefiles
+DST_MAPS_DIR = APP_CONFIG['dst_maps_dir'].blank? ? File.join(Rails.root, '/public/mapimages/dst/') : APP_CONFIG['dst_maps_dir']
+SRC_MAPS_DIR = APP_CONFIG['src_maps_dir'].blank? ? File.join(Rails.root, '/public/mapimages/src/') : APP_CONFIG['src_maps_dir']
+TILEINDEX_DIR = APP_CONFIG['tileindex_dir'].blank? ? File.join(Rails.root, '/db/maptileindex') : APP_CONFIG['tileindex_dir']
+
+#if gdal is not on the normal path
 GDAL_PATH = APP_CONFIG['gdal_path'] || ""
-#TODO
-#MAX_DIMENSION
-#MAX_ATTACHMENT_SIZE
-#GDAL_MEMORY_LIMIT
-#TILEINDEX_DIR
+
+#
+# Uncomment and populate the config file if you want to enable:
+# MAX_DIMENSION = will reduce the dimensions of the image when uploaded
+# MAX_ATTACHMENT_SIZE = will reject files that are bigger than this
+# GDAL_MEMORY_LIMIT = limit the amount of memory available to gdal 
+#
+#MAX_DIMENSION = APP_CONFIG['max_dimension']
+#MAX_ATTACHMENT_SIZE = APP_CONFIG['max_attachment_size']
+#GDAL_MEMORY_LIMIT = APP_CONFIG['gdal_memory_limit']
+
 
 ActionMailer::Base.default_url_options[:host] = APP_CONFIG['host']
 Devise.mailer_sender = APP_CONFIG['email']
