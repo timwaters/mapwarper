15425339738720b1409da7636c290b27b11fd0f2
Tim Waters <chippy2005@gmail.com>
2014-08-27 23:12:27 +0100

    add missing paths

--- config/deploy.rb
+++ config/deploy.rb
@@ -30,7 +30,7 @@ set :pty, true
 set :linked_files, %w{config/database.yml config/application.yml config/secrets.yml} 
 
 # Default value for linked_dirs is []
-set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system public/uploads public/mapimages db/maptileindex}
+set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system public/uploads public/mapimages public/mapimages/dst  public/mapimages/dst/png public/mapimages/src db/maptileindex}
 
 # Default value for default_env is {}
 # set :default_env, { path: "/opt/ruby/bin:$PATH" }
