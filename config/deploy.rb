28847810c3932828c9812097c10b45c45ed7badf
Tim Waters <chippy2005@gmail.com>
2014-08-27 23:02:01 +0100

    add uploaded image to linked dir

--- config/deploy.rb
+++ config/deploy.rb
@@ -30,7 +30,7 @@ set :pty, true
 set :linked_files, %w{config/database.yml config/application.yml config/secrets.yml} 
 
 # Default value for linked_dirs is []
-set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
+set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/systemn public/uploads}
 
 # Default value for default_env is {}
 # set :default_env, { path: "/opt/ruby/bin:$PATH" }
