c50647fb75e9fdf1344f9bf1a0ad67f411042dbf
Tim Waters <chippy2005@gmail.com>
2015-04-24 15:43:10 +0100

    add schema

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140819173754) do
-
-  # These are extensions that must be enabled in order to support this database
-  enable_extension "plpgsql"
+ActiveRecord::Schema.define(version: 20150424134733) do
 
   create_table "audits", force: true do |t|
     t.integer  "auditable_id"
@@ -169,6 +166,7 @@ ActiveRecord::Schema.define(version: 20140819173754) do
     t.integer  "map_type"
     t.string   "source_uri"
     t.spatial  "bbox_geom",              limit: {:srid=>-1, :type=>"polygon"}
+    t.integer  "placing_state"
     t.decimal  "rough_lat",                                                    precision: 15, scale: 10
     t.decimal  "rough_lon",                                                    precision: 15, scale: 10
     t.spatial  "rough_centroid",         limit: {:srid=>-1, :type=>"point"}
