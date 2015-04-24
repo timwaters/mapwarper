d5c2ffb8c0a74aed5d07702b094d17950d8a7390
Tim Waters <chippy2005@gmail.com>
2015-04-24 15:45:04 +0100

    use master for deploys now

--- config/deploy.rb
+++ config/deploy.rb
@@ -6,7 +6,7 @@ set :repo_url, 'git@github.com:timwaters/mapwarper.git'
 
 # Default branch is :master
 # ask :branch, proc { `git rev-parse --abbrev-ref HEAD`.chomp }.call
-set :branch, "rails4"
+set :branch, "master"
 
 # Default deploy_to directory is /var/www/my_app
 # set :deploy_to, '/var/www/my_app'
