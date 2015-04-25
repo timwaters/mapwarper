ba2f775f37aea9cac0311ba69e2d3d278e692e3f
Tim Waters <chippy2005@gmail.com>
2015-04-25 17:54:27 +0100

    opt into new raise_in_transational_callbacks to get rid of warnings

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -34,4 +34,5 @@ Rails.application.configure do
 
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
+  config.active_record.raise_in_transactional_callbacks = true
 end
