84bd9ac2e319621154f8a14b542ebc3ccc3c7a72
Tim Waters <chippy2005@gmail.com>
2016-07-30 16:40:41 +0100

    use http caching for unwarped wms image, for browser and passenger turbocache usage

--- app/controllers/maps_controller.rb
+++ app/controllers/maps_controller.rb
@@ -779,6 +779,9 @@ class MapsController < ApplicationController
     if status == "unwarped"
       raster.data = @map.unwarped_filename
       
+      #HTTP CACHING for unwarped image (used by passenger and browser)
+      expires_in 10.months, :public => true
+    
     else #show the warped map
       raster.data = @map.warped_filename
     end
