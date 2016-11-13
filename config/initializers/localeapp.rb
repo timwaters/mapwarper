4f116a0eb1f3f70b539b995c1e325283010f1b51
Tim Waters <chippy2005@gmail.com>
2016-11-13 19:07:42 +0000

    make the localeapp initializer run from env variable, and update locales with some missing translations

--- config/initializers/localeapp.rb
+++ config/initializers/localeapp.rb
@@ -1,8 +1,14 @@
 require 'localeapp/rails'
 
+if defined? APP_CONFIG['localeapp_api_key'] 
+  key = APP_CONFIG['localeapp_api_key']
+else
+  key = ENV['LOCALEAPP_API_KEY']
+end
+
 Localeapp.configure do |config|
   config.sending_environments = []
   config.reloading_environments = []
   config.polling_environments = []
-  config.api_key = APP_CONFIG['localeapp_api_key']
+  config.api_key = key
 end
