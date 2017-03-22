a1c7e4ecd34bd76ca6415258b9e829a0eb821efc
Tim Waters <chippy2005@gmail.com>
2017-03-22 14:22:02 +0000

    Add translations and locale stuff from nl / leiden branch

--- config/initializers/localeapp.rb
+++ config/initializers/localeapp.rb
@@ -1,14 +1,16 @@
-require 'localeapp/rails'
+if defined?(Rails) && !Rails.env.production?
+    require 'localeapp/rails'
 
-if defined? APP_CONFIG['localeapp_api_key'] 
-  key = APP_CONFIG['localeapp_api_key']
-else
-  key = ENV['LOCALEAPP_API_KEY']
-end
+    if defined? APP_CONFIG['localeapp_api_key'] 
+      key = APP_CONFIG['localeapp_api_key']
+    else
+      key = ENV['LOCALEAPP_API_KEY']
+    end
 
-Localeapp.configure do |config|
-  config.sending_environments = []
-  config.reloading_environments = []
-  config.polling_environments = []
-  config.api_key = key
+    Localeapp.configure do |config|
+      config.sending_environments = []
+      config.reloading_environments = []
+      config.polling_environments = []
+      config.api_key = key
+    end
 end
